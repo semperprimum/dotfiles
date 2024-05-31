bfb29c2029c7a9829df24ce88923e686741cc40c
semperprimum <bkeem0266+github@gmail.com>
2024-05-31 11:49:19 +0500

    update nvim and sketchybar

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,7 +16,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -582,7 +582,8 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
       },
     },
   },
--- sketchybar/items/media.sh
+++ sketchybar/items/media.sh
@@ -4,6 +4,7 @@ sketchybar --add item media right \
            --set media label.max_chars=30 \
                        icon.padding_left=0 \
                        scroll_texts=on \
+                       label.scroll_duration=200 \
                        icon=􀑪             \
                        background.drawing=off \
                        script="$PLUGIN_DIR/media.sh" \
