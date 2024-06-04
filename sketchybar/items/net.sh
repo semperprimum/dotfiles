04ea202273f7abdbd36907dcd44b07a761d89ad8
semperprimum <bkeem0266+github@gmail.com>
2024-06-04 13:19:45 +0500

    modify sketchybar

--- sketchybar/items/net.sh
+++ sketchybar/items/net.sh
@@ -1,14 +1,12 @@
-# ...
-# sketchybar --add item net right                  \
-#            --set net script="$PLUGIN_DIR/net.sh" \
-#                      updates=on                  \
-#                      label.drawing=off           \
-#                      icon.padding_right=4        \
-#            --subscribe net wifi_change
-# ...
+sketchybar --add item net right                  \
+           --set net script="$PLUGIN_DIR/net.sh" \
+                     updates=on                  \
+                     label.drawing=off           \
+                     icon.padding_right=4        \
+           --subscribe net wifi_change
 
-sketchybar --add alias "Control Center,WiFi" right             \
-           --set "Control Center,WiFi" alias.color=0xffe6db74  \
-                                       background.padding_left=-12 \
-                                       background.padding_right=-12 \
-                                       alias.scale=0.9
+# sketchybar --add alias "Control Center,WiFi" right             \
+#            --set "Control Center,WiFi" alias.color=0xffe6db74  \
+#                                        background.padding_left=-12 \
+#                                        background.padding_right=-12 \
+#                                        alias.scale=0.9
