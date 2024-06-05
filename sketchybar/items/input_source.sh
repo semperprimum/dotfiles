b8e550a4b5fde8c940d14ca7fcc1683b42ca3e71
semperprimum <bkeem0266+github@gmail.com>
2024-06-05 10:44:16 +0500

    update sketchybar

---
+++ sketchybar/items/input_source.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# -- Requires Stats.app -- 
+sketchybar --add alias "TextInputMenuAgent,Item-0" right             \
+            --set "TextInputMenuAgent,Item-0" alias.color=0xffe6db74  \
+                                       background.padding_left=-12 \
+                                       background.padding_right=-12 \
+                                       alias.scale=1
